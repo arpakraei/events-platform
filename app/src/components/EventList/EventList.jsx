c2bc04e98aace506ad11a6d37632f4f903384781
Ahmadreza Pakraei <arpakraei@gmail.com>
2026-05-08 09:14:58 +0200

    feat: add search with debounce to event list

--- app/src/components/EventList/EventList.jsx
+++ app/src/components/EventList/EventList.jsx
@@ -12,13 +12,26 @@ export default function EventList() {
   const [err, setError] = useState("");
   const [loading, setLoading] = useState(true);
   const [filterCategory, setFilterCategory] = useState("All");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [deboundSearch, setDeboundSearch] = useState("");
+
+  //serch bar
+
+  const url = `http://localhost:3001/api/events?search=${deboundSearch}`;
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDeboundSearch(searchTerm);
+    }, 500);
+    return () => clearTimeout(timer);
+  }, [searchTerm]);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         setLoading(true);
-        const response = await fetch("http://localhost:3001/api/events");
+        const response = await fetch(url);
         if (!response.ok) {
-          throw new Error("Failed To fetch data");
+          throw new Error("Can not fetch data");
         }
         const data = await response.json();
         setEvents(data);
@@ -30,41 +43,56 @@ export default function EventList() {
     };
 
     fetchData();
-  }, []);
+  }, [url]);
 
+  // filter
   const filteredEvents = events.filter((event) =>
     filterCategory === "All" ? true : event.category === filterCategory,
   );
-  if (err) {
-    return <p>{err}</p>;
-  }
-  if (loading) {
-    return <p>Data is loading...</p>;
-  } else
-    return (
-      <div>
-        <div>
-          <FilterBar
-            filterCategory={filterCategory}
-            setFilterCategory={setFilterCategory}
-          />
-        </div>
-        <div>
-          {filteredEvents.length === 0 ? (
-            <p>No events found for this category</p>
-          ) : (
-            <ul
-              className={style.cards}
-              style={{ "--numcards": filteredEvents.length }}
-            >
-              {filteredEvents.map((event, index) => (
-                <li key={event.id} style={{ "--index": index + 1 }}>
-                  <EventCard {...event} />
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
+
+  // view
+
+  return (
+    <div>
+      <div className={style.searchBar}>
+        <label htmlFor="search" className={style.label}>
+          Search
+        </label>
+        <input
+          type="text"
+          value={searchTerm}
+          id="search"
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className={style.input}
+          placeholder="Search events..."
+        />
       </div>
-    );
+      <div>
+        <FilterBar
+          filterCategory={filterCategory}
+          setFilterCategory={setFilterCategory}
+        />
+      </div>
+      <div>
+        {loading ? (
+          <p>Data is loading...</p>
+        ) : err ? (
+          <p>{err}</p>
+        ) : filteredEvents.length === 0 ? (
+          <p>No events found</p>
+        ) : (
+          <ul
+            className={style.cards}
+            style={{ "--numcards": filteredEvents.length }}
+          >
+            {filteredEvents.map((event, index) => (
+              <li key={event.id} style={{ "--index": index + 1 }}>
+                <EventCard {...event} />
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
 }
